1448bb8176af0dc793f0b5c16bce7b6c9c7644b1
tanh <tanh_eth@proton.me>
2023-03-31 23:39:51 -0400

    Update next.config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  output: "export",
+  exportTrailingSlash: true,
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
